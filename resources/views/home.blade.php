5ebe40d229179420f995c506a821e45bc938c8d0
Juan C. Bertoia <bertoia114@gmail.com>
2022-03-25 04:54:59 -0300

    armado y estilos de nav y encabezado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,23 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.NQoFPy-J3909YTr_Mw0Y5gAAAA%26pid%3DApi&f=1" class="rounded-circle" style="max-width: 170px;">
+        </div>
+        <div class="col-9 p-8">
+            <div class="pt-5">
+                <h1>freecodecamp</h1>
             </div>
+            <div class="d-flex">
+                <div class="pe-5"><strong>153</strong> post</div>
+                <div class="pe-5"><strong>23k</strong> followers</div>
+                <div class="pe-5"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-3 fw-bold ">freeCodeCamp.org</div>
+            <div>We're a global community of millions of people learning to code together.
+                LearnToCodeRPG: https://www.freecodecamp.org/news/learn-to-code-rpg/</div>
+            <div><a href="#">www.freecodecamp.org</a></div>
         </div>
     </div>
 </div>
