6bd2f6126a61294774a37a9ed69f0d3465f633c9
Juan C. Bertoia <bertoia114@gmail.com>
2022-03-31 16:52:51 -0300

    Imagen por default en perfil

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -11,6 +11,16 @@ class Profile extends Model
     use HasFactory;
     protected $guarded = [];
 
+
+    //Establecer imagen de perfil.
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : 'default/defaultProfileImage.png';
+
+        return '/storage/' . $imagePath;
+    }
+
+    // Relación uno a uno con tabla user.
     public function user()
     {
         return $this->belongsTo(User::class);
